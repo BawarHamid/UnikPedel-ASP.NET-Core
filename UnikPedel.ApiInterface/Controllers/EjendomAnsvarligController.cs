0cace29fea7c3ad136952bba035d19a9017a6746
Jamal Abed El-Naser Ghalayini <jaeg32833@edu.ucl.dk>
2022-05-13 13:28:25 +0200

    Tilføje Ejendom/EjendomAnvarlig Controller i Api fixed noget i IServiceEjendomAnsvarlig.

---
+++ UnikPedel.ApiInterface/Controllers/EjendomAnsvarligController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnikPedel.Application.Contract.Dtos;
+using UnikPedel.Application.EjendomsAnsvarligContract;
+using UnikPedel.Application.EjendomsAnsvarligContract.EjendomsAnsvarligDto;
+using UnikPedel.Contract.IServiceEjendomAnsvarlig;
+using UnikPedel.Contract.IServiceEjendomAnsvarlig.EjendomAnsvarligDtos;
+
+namespace UnikPedel.ApiInterface.Controllers
+{
+    [Route("api/EjendomAnsvarlig")]
+    [ApiController]
+
+    public class EjendomAnsvarligController : ControllerBase, IServiceEjendomAnsvarlig
+    {
+        private readonly IEjendomsAnsvarligQuery _ejendomAnsvarligQuery;
+        private readonly IEjendomsAnsvarligCommand _ejendomAnsvarligCommand;
+
+        public EjendomAnsvarligController(IEjendomsAnsvarligQuery ejendomAnsvarligQuery, IEjendomsAnsvarligCommand ejendomAnsvarligCommand)
+        {
+            _ejendomAnsvarligQuery = ejendomAnsvarligQuery;
+            _ejendomAnsvarligCommand = ejendomAnsvarligCommand; 
+        }
+        // POST api/<EjendomAnsvarligController> opretter en EjendomAnsvarlig.
+        [HttpPost]
+        public async Task CreateEjendomAnsvarligAsync(EjendomAnsvarligDto ejendomAnsvarlig)
+        {
+            await _ejendomAnsvarligCommand.CreateAsync(new EjendomsAnsvarligCommandDto
+            {
+                Id=ejendomAnsvarlig.Id,
+                ViceværtId=ejendomAnsvarlig.ViceværtId,
+                EjendomId = ejendomAnsvarlig.EjendomId
+            });
+        }
+
+        // DELETE api/<EjendomAnsvarligController>/ sletter en bestemet EejendomAnsvarlig udfra Id
+        [HttpDelete("{id}")]
+        public async Task DeleteEjendomAnsvarligAsync(Guid Id)
+        {
+            await _ejendomAnsvarligCommand.DeleteAsync(new EjendomsAnsvarligCommandDto { Id = Id });
+        }
+
+        //PUT api/<EjendomAnsvarligController>/ når man laver update på en EjendomAnsvarlig.
+        [HttpPut("{id}")]
+        public async Task EditEjendomAnsvarligAsync(EjendomAnsvarligDto ejendomAnsvarlig)
+        {
+            await _ejendomAnsvarligCommand.EditAsync(new EjendomsAnsvarligCommandDto
+            {
+                Id = ejendomAnsvarlig.Id,
+                ViceværtId = ejendomAnsvarlig.ViceværtId,
+                Vicevært= ejendomAnsvarlig.Vicevært,
+                EjendomId = ejendomAnsvarlig.EjendomId,
+                Ejendom = ejendomAnsvarlig.Ejendom
+            });
+            
+        }
+
+
+        // GET api/<EjendomController> henter en bestemt Ejendom udfra Id
+        [HttpGet("{id}")]
+        public async Task<EjendomAnsvarligDto?> GetEjendomAnsvarligAsync(Guid Id)
+        {
+            var ejendomsAnsvarlig = await _ejendomAnsvarligQuery.GetEjendomAnsvarligAsync(Id);
+            if (ejendomsAnsvarlig is null) return null;
+            return new EjendomAnsvarligDto
+            {
+                Id = ejendomsAnsvarlig.Id,
+                ViceværtId = ejendomsAnsvarlig.ViceværtId,
+                Vicevært = ejendomsAnsvarlig.Vicevært,
+                EjendomId=ejendomsAnsvarlig.EjendomId,
+                Ejendom = ejendomsAnsvarlig.Ejendom
+            };
+           
+        }
+
+
+        // GET: api/<EjendomAnsvarligController> henter en list med alle EjendomAnsvarlig
+        [HttpGet]
+        public async Task<IEnumerable<EjendomAnsvarligDto>> GetEjendomAnsvarligAsync()
+        {
+            var result = new List<EjendomAnsvarligDto>();
+            var ejendomAnsvarlig = await _ejendomAnsvarligQuery.GetEjendomsAnsvarligAsync();
+            ejendomAnsvarlig.ToList()
+                .ForEach(ejendoms => result.Add(new EjendomAnsvarligDto
+                {
+                    Id = ejendoms.Id,
+                    ViceværtId = ejendoms.ViceværtId,
+                    Vicevært = ejendoms.Vicevært,
+                    EjendomId = ejendoms.EjendomId,
+                    Ejendom = ejendoms.Ejendom
+
+                }));
+            return result;
+           
+        }
+    }
+}
---
+++ UnikPedel.ApiInterface/Controllers/EjendomController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnikPedel.Application.EjendomContract;
+using UnikPedel.Contract.IServiceEjendom;
+using UnikPedel.Contract.IServiceEjendom.EjendomDtos;
+
+namespace UnikPedel.ApiInterface.Controllers
+{
+    [Route("api/Ejendom")]
+    [ApiController]
+    public class EjendomController : ControllerBase, IServiceEjendom
+    {
+        private readonly IEjendomQuery _ejendomQuery;
+
+        public EjendomController(IEjendomQuery ejendomQuery)
+        {
+            _ejendomQuery = ejendomQuery;
+        }
+
+
+        // GET api/<EjendomController> henter en bestemt Ejendom udfra Id
+        [HttpGet("{id}")]
+        public async Task<EjendomDto?> GetEjendomAsync(Guid Id)
+        {
+            var ejendom = await _ejendomQuery.GetEjendom(Id);
+            if (ejendom is null) return null;
+            return new EjendomDto
+            {
+                Id = ejendom.Id,
+                VejNavn = ejendom.VejNavn,
+                BygningsNummer = ejendom.BygningsNummer,
+                PostNummer = ejendom.PostNummer,
+                By = ejendom.By,
+                Region = ejendom.Region,
+                LandKode = ejendom.LandKode
+
+            };
+        }
+
+        // GET: api/<EjendomController> henter en list med alle Ejendom
+        [HttpGet]
+        public async Task<IEnumerable<EjendomDto>> GetEjendomAsync()
+        {
+            var result = new List<EjendomDto>();
+            var ejendom = await _ejendomQuery.GetEjendoms();
+            ejendom.ToList().ForEach(a => result.Add(new EjendomDto
+            {
+                Id = a.Id,
+                VejNavn = a.VejNavn,
+                BygningsNummer = a.BygningsNummer,
+                PostNummer = a.PostNummer,
+                By = a.By,
+                Region = a.Region,
+                LandKode = a.LandKode
+            }));
+            return result;
+        }
+    }
+}
+
