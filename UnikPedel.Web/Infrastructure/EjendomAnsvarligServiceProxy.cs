21fc46ee809b05793af6337ebd3da7e1d7a21389
Nassir Ahmad Nowruzi <nano32868@edu.ucl.dk>
2022-05-21 22:05:01 +0200

    Opdatering på UI delen.

---
+++ UnikPedel.Web/Infrastructure/EjendomAnsvarligServiceProxy.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using UnikPedel.Contract.IServiceEjendomAnsvarlig;
+using UnikPedel.Contract.IServiceEjendomAnsvarlig.EjendomAnsvarligDtos;
+
+namespace UnikPedel.Web.Infrastructure
+{
+    public class EjendomAnsvarligServiceProxy : IServiceEjendomAnsvarlig
+    {
+        private readonly HttpClient _client;
+
+        public EjendomAnsvarligServiceProxy(HttpClient client)
+        {
+            _client = client;
+        }
+
+        async Task IServiceEjendomAnsvarlig.CreateEjendomAnsvarligAsync(EjendomAnsvarligCreateDto ejendomAnsvarlig)
+        {
+            var EjenAnsvarDtoJson = new StringContent(
+                JsonSerializer.Serialize(ejendomAnsvarlig),
+                Encoding.UTF8,
+                MediaTypeNames.Application.Json);
+            await _client.PostAsync("/api/EjendomAnsvarlig", EjenAnsvarDtoJson);
+        }
+
+        async Task IServiceEjendomAnsvarlig.DeleteEjendomAnsvarligAsync(int Id)
+        {
+            await _client.DeleteAsync($"/api/EjendomAnsvarlig/{Id}");
+        }
+
+        async Task IServiceEjendomAnsvarlig.EditEjendomAnsvarligAsync(EjendomAnsvarligDto ejendomAnsvarlig)
+        {
+            var EjenAnsvarDtoJson = new StringContent(
+               JsonSerializer.Serialize(ejendomAnsvarlig),
+               Encoding.UTF8,
+               MediaTypeNames.Application.Json);
+            await _client.PutAsync("/api/EjendomAnsvarlig", EjenAnsvarDtoJson);
+        }
+
+        async Task<EjendomAnsvarligDto?> IServiceEjendomAnsvarlig.GetEjendomAnsvarligAsync(int Id)
+        {
+            return await _client.GetFromJsonAsync<EjendomAnsvarligDto>($"/api/EjendomAnsvarlig/{Id}");
+        }
+
+        async Task<IEnumerable<EjendomAnsvarligDto>> IServiceEjendomAnsvarlig.GetEjendomAnsvarligAsync()
+        {
+            return await _client.GetFromJsonAsync<IEnumerable<EjendomAnsvarligDto>>($"api/EjendomAnsvarlig");
+        }
+    }
+}
--- UnikPedel.Web/Pages/Admin/Afdelinger/Index.cshtml.cs
+++ UnikPedel.Web/Pages/Admin/Afdelinger/Index.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using UnikPedel.Contract.IServiceEjendomAnsvarlig;
+using UnikPedel.Contract.IServiceEjendomAnsvarlig.EjendomAnsvarligDtos;
 
 namespace UnikPedel.Web.Pages.Afdelinger
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IServiceEjendomAnsvarlig _serviceEjendomAnsvarlig;
+
+        public IndexModel(IServiceEjendomAnsvarlig serviceEjendomAnsvarlig)
         {
+            _serviceEjendomAnsvarlig = serviceEjendomAnsvarlig;
+        }
+
+        [BindProperty]
+        public IEnumerable<EjendomAnsvarligGetAll> EjendomAnsvarlig { get; set; } = Enumerable.Empty<EjendomAnsvarligGetAll>();
+        public async Task OnGetAsync()
+        {
+            var ejenAnsvarlig = new List<EjendomAnsvarligGetAll>();
+            var dbEjenAnsvarlig = await _serviceEjendomAnsvarlig.GetEjendomAnsvarligAsync();
+            dbEjenAnsvarlig.ToList().ForEach(x => ejenAnsvarlig.Add(new EjendomAnsvarligGetAll(x)));
+            EjendomAnsvarlig = ejenAnsvarlig;
+        }
+
+        public class EjendomAnsvarligGetAll
+        {
+            public int Id { get; set; }
+            public int ViceværtId { get; set; }
+            public int EjendomId { get; set; }
+            public EjendomAnsvarligGetAll(EjendomAnsvarligDto ejendomAnsvarlig)
+            {
+                Id = ejendomAnsvarlig.Id;
+                ViceværtId = ejendomAnsvarlig.ViceværtId;
+                EjendomId = ejendomAnsvarlig.EjendomId;
+
+            }
         }
     }
 }
