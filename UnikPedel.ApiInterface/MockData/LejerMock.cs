4f0dc0236c560b696cb122d12df414bfa4a296bf
Nassir Ahmad Nowruzi <nano32868@edu.ucl.dk>
2022-05-25 20:22:22 +0200

    Landing page og små rettelser på UI

--- UnikPedel.ApiInterface/MockData/LejerMock.cs
+++ UnikPedel.ApiInterface/MockData/LejerMock.cs
@@ -4,10 +4,10 @@ namespace UnikPedel.ApiInterface.MockData
 {
     public class LejerMock
     {
-        [Get("/Lejer/{id}")] Task<LejerResponse?> GetLejer([AliasAs("id")] Guid lejerId);
-        [Put("/Lejer/{id}")] Task<LejerResponse?> UpdateLejer([AliasAs("id")] Guid lejerId, LejerUpdateRequest request);
-        [Delete("/Lejer/{id}")] Task DeleteLejer([AliasAs("id")] Guid lejerId);
-        [Get("/Lejemaal/{id}/Lejer")] Task<IEnumerable<LejerResponse>?> ListLejereOnLejemaal([AliasAs("id")] Guid lejemaalId);
-        [Post("/Lejer")] Task<LejerResponse?> CreateLejer(LejerCreateRequest request);
+        //[Get("/Lejer/{id}")] Task<LejerResponse?> GetLejer([AliasAs("id")] Guid lejerId);
+        //[Put("/Lejer/{id}")] Task<LejerResponse?> UpdateLejer([AliasAs("id")] Guid lejerId, LejerUpdateRequest request);
+        //[Delete("/Lejer/{id}")] Task DeleteLejer([AliasAs("id")] Guid lejerId);
+        //[Get("/Lejemaal/{id}/Lejer")] Task<IEnumerable<LejerResponse>?> ListLejereOnLejemaal([AliasAs("id")] Guid lejemaalId);
+        //[Post("/Lejer")] Task<LejerResponse?> CreateLejer(LejerCreateRequest request);
     }
 }
