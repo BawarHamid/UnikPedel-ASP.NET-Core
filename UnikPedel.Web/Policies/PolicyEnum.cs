4933775df49d3394fe772121de6309c99afbc6e1
Nikolaos Chanania <nich32832@edu.ucl.dk>
2022-05-28 19:19:16 +0200

    authorazation accounts

--- UnikPedel.Web/Policies/PolicyEnum.cs
+++ UnikPedel.Web/Policies/PolicyEnum.cs
@@ -4,5 +4,6 @@
     {
         public const string AdminOnly = "AdminOnly";
         public const string ViceVærtOnly = "ViceværtOnly";
+        public const string LejerOnly = "LejerOnly";
     }
 }
